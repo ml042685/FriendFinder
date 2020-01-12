5ee05c4bef4f5239cff58dcbf1d457324b358e83
Marcos Luna <ml042685@yahoo.com>
2020-01-11 23:51:29 -0600

    Friend Finder3

---
+++ server.js
@@ -0,0 +1,25 @@
+//Dependencies:
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/html-routes.js")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
